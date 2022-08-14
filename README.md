570b2a47519d9f399bc61a2a03d68fdfd0396194
suzukiyouichi <yo1sjp@outlook.jp>
2022-08-15 08:52:00 +0900

    add i2c_rotary_encoder

---
+++ README.md
@@ -0,0 +1,84 @@
+# I2C ロータリーエンコーダ
+
+<img src="/images/pcb_front.png" height="200">
+<br><br><br>
+
+
+## 説明
+
+I2CロータリエンコーダはATTiny202を利用したロータリエンコーダの操作をI2C越しに読み込みをするための基板です。
+
+<br><br>
+
+
+## ATTiny202 ピン
+
+<img src="/images/connect.png" height="900">
+
+<table>
+  <tr>
+    <th>番号</th>
+    <th>ピン</th>
+    <th>接続</th>
+    <td rowspan="5">　</td>
+    <th>番号</th>
+    <th>ピン</th>
+    <th>接続</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>VDD</td>
+    <td>VCC</td>
+    <td>8</td>
+    <td>GND</td>
+    <td>GND</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>PA6</td>
+    <td>rotary0_A</td>
+    <td>7</td>
+    <td>PA3</td>
+    <td>rotary1_A</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>PA7</td>
+    <td>rotary0_B</td>
+    <td>6</td>
+    <td>PA0</td>
+    <td>rotary1_B</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>PA1</td>
+    <td>I2C SDA</td>
+    <td>5</td>
+    <td>PA2</td>
+    <td>I2C SCL</td>
+  </tr>
+</table>
+<br><br>
+
+
+
+## アドレス設定方法
+
+プログラムの中に固定値でアドレスを指定しているため、ビルドして書込みを行わない限りアドレスの変更はできません。<br>
+デフォルトで 0x45 が書き込まれています。<br>
+<br><br>
+
+
+## 開発環境の作り方
+
+<a href="https://ameblo.jp/pta55/entry-12654450554.html" target="_blank">https://ameblo.jp/pta55/entry-12654450554.html</a>
+<br><br>
+
+
+## 詳細情報
+
+電圧：　1.8V　～　5.5V  
+信号：　I2C　（～1.7MHz）
+<br><br>
+
+
---
+++ software/i2c_rotary_encorder/i2c_rotary_encorder.ino
@@ -0,0 +1,68 @@
+// ロータリーエンコーダーの入力をI2Cに流す
+
+// 開発環境の作り方
+// https://ameblo.jp/pta55/entry-12654450554.html
+
+#include <Wire.h>
+#include <avr/pgmspace.h> 
+
+// 本体のアドレス
+#define I2C_SLAVE_ADD 0x45
+
+uint8_t key_input = 0x00;
+uint8_t old_input = 0x00;
+uint8_t send_input = 0x00;
+
+// I2Cデータ要求を受け取った時の処理
+void requestEvent();
+
+void setup() {
+    // ロータリーエンコーダ読み込みピンの初期化
+    pinMode(A0, INPUT_PULLUP);
+    pinMode(A3, INPUT_PULLUP);
+    pinMode(A7, INPUT_PULLUP);
+    pinMode(A6, INPUT_PULLUP);
+  
+    // I2C スレーブ初期化
+    Wire.begin(I2C_SLAVE_ADD);
+    Wire.onRequest(requestEvent);
+
+}
+
+// I2Cデータ要求を受け取った時の処理
+void requestEvent() {
+    Wire.write(send_input);
+    send_input = 0x00;
+}
+
+void loop() {
+  // key read
+  old_input = key_input;
+
+  // 今回分を読み取り
+  key_input = 0x00;
+  if (digitalRead(A0)) key_input |= 0x01;
+  if (digitalRead(A3)) key_input |= 0x02;
+  if (digitalRead(A6)) key_input |= 0x04;
+  if (digitalRead(A7)) key_input |= 0x08;
+
+  // ロータリーエンコーダー１
+  if ((old_input & 0x03) == 0x03) { // 前の読み取りが AB両方ONだった
+      if ((key_input & 0x01) && !(key_input & 0x02)) { // 今回Aのみ
+          send_input |= 0x01;
+      } else if (!(key_input & 0x01) && (key_input & 0x02)) { // 今回Bのみ
+          send_input |= 0x02;
+      }
+  }
+
+  // ロータリーエンコーダー２
+  if ((old_input & 0x0c) == 0x0c) { // 前の読み取りが AB両方ONだった
+      if ((key_input & 0x04) && !(key_input & 0x08)) { // 今回Aのみ
+          send_input |= 0x04;
+      } else if (!(key_input & 0x04) && (key_input & 0x08)) { // 今回Bのみ
+          send_input |= 0x08;
+      }
+  }
+
+  delay(2);
+}
